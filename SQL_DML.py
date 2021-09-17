85aa087970c7699d5ddc57e991ced54f2eb76159
KCK <ajayingle109@gmail.com>
2021-09-17 17:11:01 +0530

    add

--- SQL_DML.py
+++ SQL_DML.py
@@ -8,10 +8,10 @@ mydb = mysql.connector.connect(
     charset= "utf8",
     database= "poonam"
 )
-
+#DELETE RECORD FROM TABLE
 
 mycursor = mydb.cursor()
 b=mycursor.execute('USE poonam')
-a=mycursor.execute("UPDATE company SET SALARY = 50000 WHERE SALARY = 15000")
+a=mycursor.execute("DELETE FROM company WHERE NAME= 'Jagu'")
 print(b,a)
 mydb.commit()
